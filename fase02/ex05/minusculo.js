ca606b8f74fb6fd96dd5e9d20a0f8b839d364e87
rodsiqueirabh <44844081+rodsiqueirabh@users.noreply.github.com>
2019-07-10 21:09:42 -0300

    Correção do exercício5

--- fase02/ex05/minusculo.js
+++ fase02/ex05/minusculo.js
@@ -1,10 +1,9 @@
 function minusculo(string) {
     
-    var palavra = string;
-
-    var palavra1 = palavra.substr(0,1);	
-    palavra = palavra.replace(palavra1, palavra1.toLowerCase());
-    return palavra;
+    var palavra1 = string.substr(0,1);	
+    return palavra1.toLowerCase();
 }
+
+
 
 
