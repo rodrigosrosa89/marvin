1b5635404547281b3bc4312881df941988ceb5ca
rodsiqueirabh <44844081+rodsiqueirabh@users.noreply.github.com>
2019-07-16 00:34:47 -0300

    Exercício 2: Os números pares

---
+++ fase04/ex02/pares.js
@@ -0,0 +1,8 @@
+function pares(n1, n2) {
+    if ((n1 % 2 === 0) && (n2 % 2 === 0)) {
+        return "Os números são p a r e s";
+    } else {
+        return "Os números não são pares"
+    }
+}
+
