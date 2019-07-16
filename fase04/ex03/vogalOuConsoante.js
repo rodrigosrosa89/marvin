3297e54e87a3570080080914b424c3bccd0db7a0
rodsiqueirabh <44844081+rodsiqueirabh@users.noreply.github.com>
2019-07-16 00:51:56 -0300

    Exercício 03: Vogal ou consoante?

---
+++ fase04/ex03/vogalOuConsoante.js
@@ -0,0 +1,9 @@
+function vogalOuConsoante(letra) {
+    var l = letra.toLowerCase();
+    if ( l == "a" || l == "e" || l == "i" || l =="o" || l=="u") {
+        return "Vogal";
+    } else {
+        return "Consoante";
+    }
+}
+
