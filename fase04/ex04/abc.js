254519982ef045939e652525c769030aa7206d0a
rodsiqueirabh <44844081+rodsiqueirabh@users.noreply.github.com>
2019-07-16 00:58:18 -0300

    Exercício 04: O começo do alfabeto

---
+++ fase04/ex04/abc.js
@@ -0,0 +1,9 @@
+function abc(string) {
+    var primeira = string.substr(0,1);
+    if (primeira == "a" || primeira == "b" || primeira == "c") {
+        return true;
+    } else {
+        return false;
+    }
+}
+
