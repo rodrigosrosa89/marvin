a23f0e48cb5d4996177cafccd2b74b2032c5a8f1
rodsiqueirabh <44844081+rodsiqueirabh@users.noreply.github.com>
2019-07-16 21:31:02 -0300

    Correção

--- fase04/ex02/pares.js
+++ fase04/ex02/pares.js
@@ -1,8 +1,8 @@
 function pares(n1, n2) {
     if ((n1 % 2 === 0) && (n2 % 2 === 0)) {
-        return "Os números são p a r e s";
+        return "Os nÃºmeros sÃ£o pares";
     } else {
-        return "Os números não são pares"
+        return "Os nÃºmeros nÃ£o sÃ£o pares"
     }
 }
 
