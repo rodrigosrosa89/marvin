5108839614dc82785a3e2b826a6ac5c803b20929
rodsiqueirabh <44844081+rodsiqueirabh@users.noreply.github.com>
2019-07-24 20:10:27 -0300

    Exercício 0: Aprendendo a contar

---
+++ fase06/ex00/entreNumeros.js
@@ -0,0 +1,14 @@
+function entreNumeros(n1,n2) {
+    if (n2 > n1) {
+    while (n1 <= n2) {
+        console.log(n1);
+         n1++;
+    }
+    } else {
+        while (n2 <= n1) {
+        console.log(n2);
+         n2++;
+    }
+    }    
+}
+
