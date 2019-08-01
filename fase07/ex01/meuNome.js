256e0e0ca7f1d4e36238b584ffbb50fc434a87d7
rodsiqueirabh <44844081+rodsiqueirabh@users.noreply.github.com>
2019-08-01 20:01:44 -0300

    Exercício 01: Olha esse nome

---
+++ fase07/ex01/meuNome.js
@@ -0,0 +1,7 @@
+function meuNome(nome) { 
+    var primeiraLetra = nome.substr(0,1).toUpperCase();
+    var outrasLetras = nome.substr(1,).toLowerCase();
+    return primeiraLetra+outrasLetras;
+    
+}
+
