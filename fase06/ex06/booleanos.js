6b29443b9d930b91b11f2d6cb989a385add4a7f7
rodsiqueirabh <44844081+rodsiqueirabh@users.noreply.github.com>
2019-07-31 21:20:22 -0300

    Correção Commit

--- fase06/ex06/booleanos.js
+++ fase06/ex06/booleanos.js
@@ -3,8 +3,10 @@ function booleanos(a){
     var contador = a.length -1;
     while (contador >= 0) {
         var x = a[contador];
+        if ((x != "true") && (x != "false")) { 
         if (x.toString() == "true" || x.toString() == "false"){
             a.splice(contador, 1);   
+        }
         }
         contador--;
     }
