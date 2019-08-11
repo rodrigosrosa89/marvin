c002a28d013aa9beb67acaee22eb6c15cdeb81c9
rodsiqueirabh <44844081+rodsiqueirabh@users.noreply.github.com>
2019-08-11 01:57:45 -0300

    Exercício 03: Nós amamos animais

--- fase07/ex03/animais.js
+++ fase07/ex03/animais.js
@@ -1,15 +1,16 @@
 function animais (array) {
     var obj = new Object();
+    obj = array;
     var novaIdade = 5;
 
     for (var i = 0; i< array.length; i++) {
-    var posicao = array[i]
+    var posicao = obj[i];
     if (posicao[idade] != null) {
         
         posicao[idade] = novaIdade;
     }
     if (posicao[altura] != null) {
-        delete array[altura]
+        delete array[altura];
     }
     novaIdade++;
     }
