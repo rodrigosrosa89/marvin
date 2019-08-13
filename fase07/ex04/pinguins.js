374b89eef2b34f49dc51dfb3a8e90a2baf0683e8
rodsiqueirabh <44844081+rodsiqueirabh@users.noreply.github.com>
2019-08-13 00:12:30 -0300

    Correção

--- fase07/ex04/pinguins.js
+++ fase07/ex04/pinguins.js
@@ -1,9 +1,10 @@
 function pinguins(arrayObj) {
     var i = 0 
-    while ( i < arrayObj.length) { 
+    while ( i < arrayObj.length) {
+        posicao = arrayObj[i];
         posicao.push({cabeca: "oculos de sol", camisa:'jaqueta polar', pes: "tenis"});
         i++;
     }
-    return arrayObt;
+    return arrayObj;
     
 }
