8c35f961381ba274e69250426f344bf339e11652
rodsiqueirabh <44844081+rodsiqueirabh@users.noreply.github.com>
2019-08-12 23:48:46 -0300

    Exercício 04: Os pinguins também são estilosos

---
+++ fase07/ex04/pinguins.js
@@ -0,0 +1,10 @@
+function pinguins(arrayObj) {
+    
+    for(var i = 0; i < arrayObjt.length; i++) {
+        var posicao = arrayObjt[i];
+        posicao.push({cabeca: "oculos de sol", camisa:'jaqueta polar', pes: "tenis"});
+        i++
+    }
+    return arrayObt;
+    
+}
